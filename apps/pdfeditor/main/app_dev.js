d3b578b52dce206c2b9530995f0079bac6bf5442
nikita_bartoshuk <nikita.bartoshuk@onlyoffice.com>
2026-04-20 11:59:46 +0300

    moved remove meta dlg in post launch

--- apps/pdfeditor/main/app_dev.js
+++ apps/pdfeditor/main/app_dev.js
@@ -238,7 +238,8 @@ require([
                     'pdfeditor/main/app/view/ShapeSettingsAdvanced',
                     'pdfeditor/main/app/view/TableSettingsAdvanced',
                     'pdfeditor/main/app/view/FormatSettingsDialog',
-                    'pdfeditor/main/app/view/ChartSettingsAdvanced'
+                    'pdfeditor/main/app/view/ChartSettingsAdvanced',
+                    'pdfeditor/main/app/view/RemoveMetaDlg',
                 ];
 
                 window.compareVersions = true;
