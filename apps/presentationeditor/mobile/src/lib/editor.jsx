b39f7a5d7b8023ff4f6fe0830f63c13c2559eac9
n-goncalves <n-goncalves@users.noreply.github.com>
2026-06-13 14:10:52 +0200

    DE/PE mobile: Fix getEditCommentControllers using already added controllers
    Signed-off-by: n-goncalves <n-goncalves@users.noreply.github.com>

--- apps/presentationeditor/mobile/src/lib/editor.jsx
+++ apps/presentationeditor/mobile/src/lib/editor.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { Link } from 'framework7-react';
 import { Device } from '../../../../common/mobile/utils/device';
-import { CommentsController, ViewCommentsController } from '../../../../common/mobile/lib/controller/collaboration/Comments';
+import { AddCommentController, EditCommentController } from '../../../../common/mobile/lib/controller/collaboration/Comments';
 import {
     PlatformIcon,
     buildFocusObjectGetters,
@@ -228,12 +228,12 @@ export const updateChartStyles = (storeChartSettings, storeFocusObjects) => {
 
 /**
  * Renders comment controller components for editing mode
- * @returns {JSX.Element} Fragment containing CommentsController and ViewCommentsController
+ * @returns {JSX.Element} Fragment containing AddCommentController and EditCommentController
  */
 export const getEditCommentControllers = () => (
     <Fragment>
-        <CommentsController />
-        <ViewCommentsController />
+        <AddCommentController />
+        <EditCommentController />
     </Fragment>
 );
 
